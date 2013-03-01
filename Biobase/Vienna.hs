fc948313b45171ce6499034e70951727386733db
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-03-01 20:39:54 +0100

    work in progress on new BiobaseVienna

--- Biobase/Vienna.hs
+++ Biobase/Vienna.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 -- | The Vienna 2004 model is a mirror of the Turner data structure. The
 -- difference is that The ViennaRNA package uses an explicit annotation for RNA
@@ -9,6 +11,26 @@
 
 module Biobase.Vienna where
 
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Generic.Mutable as VGM
+import Data.Primitive.Types
+
+import Biobase.Turner
+
+
+
+newtype Deka = Deka Int
+  deriving (Eq,Ord,Num,Read,Show)
+
+deriving instance Prim Deka
+deriving instance VGM.MVector VU.MVector Deka
+deriving instance VG.Vector VU.Vector Deka
+deriving instance VU.Unbox Deka
+
+viennaModel = emap (\(Energy e) -> Deka $ round $ 100 * e)
+
+{-
 import Data.Array.Repa.Index
 import Data.Array.Repa.Shape
 import Data.ByteString (ByteString)
@@ -63,4 +85,6 @@ data Vienna2004 = Vienna2004
   , termAU :: Int
   , intermolecularInit :: Int
   } -- deriving (Read,Show)
+
+-}
 
